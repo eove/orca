1f1605390547c7e58dd4eaa75436d6131b4858b4
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2025-10-09 22:35:30 +0200

    Backup cannot be changed by orca user

--- src/scripts/orca-protocol/sudoer/backup.nix
+++ src/scripts/orca-protocol/sudoer/backup.nix
@@ -3,7 +3,6 @@
 set -e
 seal
 VAULT_STORAGE_PATH=${config.services.vault.storagePath}
-ORCA_FOLDER="$VAULT_STORAGE_PATH/orca"
 cd $VAULT_STORAGE_PATH
 mv audit.log audit_$(date +%F_%T).log
 
@@ -14,9 +13,9 @@ TMP_DIR="$(mktemp -d)"
 cd "$TMP_DIR"
 tar --same-owner -xf "$VAULT_BACKUP" -C .
 
-mv $VAULT_BACKUP $ORCA_FOLDER
-
 C_VAULT=$(find . -type f -exec sha256sum -b {} \; | sort -k2 | sha256sum - | cut -d " " -f 1)
 echo "Cvault: $C_VAULT" | qrencode -t utf8 -i
 echo "Cvault: $C_VAULT"
+
+mv $VAULT_BACKUP $VAULT_STORAGE_PATH
 ''
