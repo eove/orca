c3e85c5da076fe461e487969e48048435b7994df
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2025-10-16 16:08:23 +0200

    Wipe only outputs

--- src/scripts/orca-protocol/sudoer/wipe_everything.nix
+++ src/scripts/orca-protocol/sudoer/wipe_everything.nix
@@ -2,7 +2,7 @@
 let
   wipe = lib.getExe pkgs.wipe;
   seal = lib.getExe all_scripts.orca_scripts.orca_user.seal;
-  inherit (config.environment.variables) VAULT_STORAGE_PATH RECORDINGS_FOLDER;
+  inherit (config.environment.variables) OUTPUT_FOLDER;
 in
 ''
   echo "Something went wrong"
@@ -10,15 +10,8 @@ in
   echo "Sealing the vault"
   ${seal}
 
-  echo "Wiping everything"
-  KEPT_LOGS=/tmp/kept_logs
-  mkdir -p $KEPT_LOGS/recordings
-  find ${VAULT_STORAGE_PATH} -iname "audit*" -exec cp -r {} $KEPT_LOGS \;
-  cp -r ${RECORDINGS_FOLDER}/* $KEPT_LOGS/recordings
-  ${wipe} -r ${VAULT_STORAGE_PATH}/*
-  mkdir -p ${VAULT_STORAGE_PATH} ${RECORDINGS_FOLDER}
-  mv $KEPT_LOGS/recordings/* ${RECORDINGS_FOLDER}
-  find $KEPT_LOGS -iname "audit*" -exec cp -r {} ${VAULT_STORAGE_PATH}  \;
+  echo "Wiping outputs"
+  ${wipe} -r ${OUTPUT_FOLDER}/*
 
   echo -e "\nPlease recreate the orca stick once the issue has been fixed\n"
 ''
