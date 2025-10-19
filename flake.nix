edda882ace98f8de6bf3cdb9d5e7d22897629fc9
FaustXVI <1016863+FaustXVI@users.noreply.github.com>
2025-10-19 21:19:42 +0200

    Merge flakes

--- flake.nix
+++ flake.nix
@@ -1,20 +1,48 @@
 {
-  description = "O.R.CA development environment";
+  description = "O.R.CA";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    ({
+      nixosModules = import ./src/orca-nixos-module.nix inputs;
+    }) //
     (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         };
+        ORCA_DISK_NAME = "VAULT_WRITABLE";
       in
       {
+        packages = {
+          iso-offline = import ./src/orca-iso.nix (inputs // { inherit system pkgs ORCA_DISK_NAME; });
+        };
+        apps = {
+          create-usb-vault = import ./src/create-usb.nix { inherit pkgs ORCA_DISK_NAME; isoImage = self.packages.${system}.iso-offline; };
+          default =
+            let
+              vmScript = pkgs.writeShellScriptBin "vmScript" ''
+                export VAULT_WRITABLE_DISK="$(pwd)/orca-testing-disk.raw"
+                if [ ! -f "$VAULT_WRITABLE_DISK" ]; then
+                  echo "Creating a new disk at $VAULT_WRITABLE_DISK"
+                  ${pkgs.qemu}/bin/qemu-img create -f raw "$VAULT_WRITABLE_DISK" 500M
+                  mkfs.ext4 -L "${ORCA_DISK_NAME}" -F "$VAULT_WRITABLE_DISK"
+                else
+                  echo "Using existing $VAULT_WRITABLE_DISK"
+                fi
+                ${pkgs.lib.getExe (import ./src/orca-vm.nix (inputs // { inherit system pkgs ORCA_DISK_NAME;}))}
+              '';
+            in
+            {
+              type = "app";
+              program = "${pkgs.lib.getExe vmScript}";
+            };
+        };
         devShells.default = pkgs.mkShell (
           let
             md-to-html = pkgs.writeShellScriptBin "md-to-html" ''
