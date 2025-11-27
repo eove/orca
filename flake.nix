dff105daddeb62089e8e26280c07568a74935915
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2025-11-27 11:02:21 +0100

    Create a github action to publish documentation

--- flake.nix
+++ flake.nix
@@ -40,73 +40,82 @@
               program = "${pkgs.lib.getExe vmScript}";
             };
         };
-        devShells.default = pkgs.mkShell (
-          let
-            md-to-html = pkgs.writeShellScriptBin "md-to-html" ''
-              if [ "$#" -eq 0 ]; then
-                echo "Usage : $0 /path/to/file.md [title]" >&2
-                exit -1
-              fi
-              echo "$0" >&2
-              DIR="$(mktemp -d)"
-              mkdir -p "$DIR"
-              MD="$1"
-              FILE=$(basename "$MD")
-              TITLE="$FILE"
-              if [ "$#" -ge 2 ]; then
-                TITLE=$(echo "$2" | tr '[]"<>' '_')
-              fi
+        devShells = {
+          default = pkgs.mkShell (
+            let
+              md-to-html = pkgs.writeShellScriptBin "md-to-html" ''
+                if [ "$#" -eq 0 ]; then
+                  echo "Usage : $0 /path/to/file.md [title]" >&2
+                  exit -1
+                fi
+                echo "$0" >&2
+                DIR="$(mktemp -d)"
+                mkdir -p "$DIR"
+                MD="$1"
+                FILE=$(basename "$MD")
+                TITLE="$FILE"
+                if [ "$#" -ge 2 ]; then
+                  TITLE=$(echo "$2" | tr '[]"<>' '_')
+                fi
 
-              cp -r ${./.}/docs/.style "$DIR"/.style >/dev/null
-              chmod +w -R "$DIR"/.style
-              cp "$MD" "$DIR/$FILE"
-              cat <<EOF > "$DIR/book.toml"
-              [book]
-              language = "en"
-              src = "."
-              title = "$TITLE"
+                cp -r ${./.}/docs/.style "$DIR"/.style >/dev/null
+                chmod +w -R "$DIR"/.style
+                cp "$MD" "$DIR/$FILE"
+                cat <<EOF > "$DIR/book.toml"
+                [book]
+                language = "en"
+                src = "."
+                title = "$TITLE"
                 
-              [preprocessor.index]
+                [preprocessor.index]
                 
-              [preprocessor.links]
+                [preprocessor.links]
                 
-              [preprocessor.alerts]
+                [preprocessor.alerts]
                 
-              [preprocessor.mermaid]
-              command = "mdbook-mermaid"
+                [preprocessor.mermaid]
+                command = "mdbook-mermaid"
                 
-              [output.html]
-              additional-js = [".style/mermaid.min.js", ".style/mermaid-init.js"]
-              additional-css = [".style/custom.css"]
+                [output.html]
+                additional-js = [".style/mermaid.min.js", ".style/mermaid-init.js"]
+                additional-css = [".style/custom.css"]
 
-              [output.html.search]
-              enable = false
+                [output.html.search]
+                enable = false
                 
-              EOF
+                EOF
 
-              cat <<EOF > "$DIR/SUMMARY.md"
-              [$TITLE](./$FILE)
-              EOF
+                cat <<EOF > "$DIR/SUMMARY.md"
+                [$TITLE](./$FILE)
+                EOF
 
-              mdbook build "$DIR" >&2
+                mdbook build "$DIR" >&2
 
-              PATH="$PATH:${pkgs.nodejs}/bin" npx --yes inliner -m "$DIR/book/index.html"
+                PATH="$PATH:${pkgs.nodejs}/bin" npx --yes inliner -m "$DIR/book/index.html"
 
-              rm -rf "$DIR/"
-            '';
-          in
-          {
+                rm -rf "$DIR/"
+              '';
+            in
+            {
+              packages = with pkgs; [
+                vault-bin
+                jq
+                openssl
+                mdbook
+                mdbook-alerts
+                mdbook-mermaid
+                md-to-html
+                asciinema
+              ];
+            }
+          );
+          ci = pkgs.mkShell {
             packages = with pkgs; [
-              vault-bin
-              jq
-              openssl
               mdbook
               mdbook-alerts
               mdbook-mermaid
-              md-to-html
-              asciinema
             ];
-          }
-        );
+          };
+        };
       }));
 }
