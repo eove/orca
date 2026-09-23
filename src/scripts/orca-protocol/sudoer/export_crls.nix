518d0d91864514d492409c9e055ade063608c95a
Bastien Diederichs <bastien.diederichs@eove.fr>
2026-09-23 11:27:22 +0200

    Missed one

--- src/scripts/orca-protocol/sudoer/export_crls.nix
+++ src/scripts/orca-protocol/sudoer/export_crls.nix
@@ -6,7 +6,7 @@ in
   set -eo pipefail
   echo 'Exporting CRLs' >&2
   PKIS=$(vault secrets list -format=json | jq -r 'to_entries[] | select(.value.type=="pki") | .key')
-  
+
   for PKI_NAME in $PKIS
   do
       PKI_NAME=''${PKI_NAME%/}
@@ -20,9 +20,7 @@ in
               mkdir -p $CRL_DIR
               vault read -format=raw ''${PKI_NAME}/issuer/$ISSUER_ID/der > $PKI_AIA_DIR/der
               # We use `/crl/pem` instead of `/crl/der`, as the latter seems to show inconsistent behavior in its output, see https://github.com/hashicorp/vault/issues/32018
-              vault read -format=raw ''${PKI_NAME}/issuer/$ISSUER_ID/crl/pem > $CRL_DIR/pem
-              openssl crl -outform der -in $CRL_DIR/pem -out $CRL_DIR/der
-              rm $CRL_DIR/pem
+              vault read -format=raw ''${PKI_NAME}/issuer/$ISSUER_ID/crl/pem | openssl crl -outform der -out $CRL_DIR/der
           fi
       done
   done
